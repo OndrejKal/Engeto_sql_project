95ddd0040f8d7d4fa96a5d0ab770317e878288fb
OndrejKal <132574358+OndrejKal@users.noreply.github.com>
2024-01-22 02:08:05 +0100

    Add files via upload

---
+++ t_ondrej_kalvas_project_SQL_secondary_final.sql
@@ -0,0 +1,20 @@
+SELECT *
+FROM countries c 
+
+SELECT *
+FROM economies e  
+
+CREATE OR REPLACE TABLE t_ondrej_kalvas_project_SQL_secondary_final AS
+	SELECT e.country,
+		e.year,
+		GDP,
+		e.population,
+		e.gini
+	FROM economies e 
+	JOIN countries c 
+		ON c.country = e.country
+	WHERE c.continent = 'Europe'
+	AND e.year BETWEEN 2006 AND 2018
+	ORDER BY country, year
+
+	
